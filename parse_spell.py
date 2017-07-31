bd3666a4986b286ea42153c0d01ec7ffe254c506
Tomi Lamsa <lamsatom@gmail.com>
2017-07-31 15:59:01 +0300

    updated parse_spell to handle rogue things better, slick feint now added properly, momentum is only added to castingTime (though code for adding it after rechargeTime is still commented too)

--- parse_spell.py
+++ parse_spell.py
@@ -12,8 +12,6 @@ import re, json, uuid, unicodedata, os, sys, codecs
 '''
 def convert(fileName, targetFileName):
 
-		
-		
 	fileName = os.path.join("data", fileName)
 	print(fileName)
 	with open(fileName) as f:
@@ -29,7 +27,7 @@ def convert(fileName, targetFileName):
 			spell.append("spellLevel: " + str(spellLevel))
 			allSpells.append(spell)
 			spell = []
-		elif "Level Spells" in line or "Level Battle Cries" in line or "Level Songs" in line or "Maneuvers" in line:
+		elif "Level Spells" in line or "Level Battle Cries" in line or "Level Songs" in line or "Maneuvers" in line or "Powers" in line:
 			spellLevel = line
 			#print("Spell level found!" + spellLevel)
 			#add an extra empty line, we depend on line number to get spell name (since the line has no other identifier), hopefully it works.
@@ -38,6 +36,7 @@ def convert(fileName, targetFileName):
 			spell.append(line)
 	
 	#add the last spell as well
+	spell.append("spellLevel: " + str(spellLevel))
 	allSpells.append(spell)
 	
 	#first write the opening bracket to the file
@@ -72,10 +71,10 @@ def parse_spell(lines):
 		
 	#print(lines)
 	
-	dict = {}
+	powerDictionary = {}
 	#set name first
-	dict["name"] = cleanUnicodeFromString(lines[1])
-	uprint(dict['name'])
+	powerDictionary["name"] = cleanUnicodeFromString(lines[1])
+	uprint(powerDictionary['name'])
 	
 	i = 1
 	for line in lines:
@@ -84,118 +83,149 @@ def parse_spell(lines):
 		#if it is ranged spell, the recharge time is on same row after symbol ;
 		if "Close-quarters spell" in line:
 			#print ("close-quarters spell")
-			dict["attackType"] = line
+			powerDictionary["attackType"] = line
 			#dict["rechargeTime"] = lines[5]
 		elif "Ranged spell" in line:
 			print("ranged spell")
-			dict["attackType"] = "Ranged spell"
-			dict["rechargeTime"] = re.sub(r'.*;', '', lines[3])
-			
-		#if line.startswith("Recharge", 0, 8)
+			powerDictionary["attackType"] = "Ranged spell"
+			powerDictionary["rechargeTime"] = re.sub(r'.*;', '', lines[3])
+		#add momentum to rechargetime so later we will know to add momentum to the rechargetime row
+		#if line.startswith("Momentum", 0, 8):
+		#	powerDictionary["rechargeTime"] = "momentum"
 		
 		#Set the recharge time. It might also have been set above, since for ranged spells recharge time is written on the same row.
 		if line.startswith("Daily", 0, 5) or line.startswith("Recharge", 0, 8) or line.startswith("At-Will", 0, 7) or line.startswith("Cyclic", 0, 6):
 			line = cleanUnicodeFromString(line)
-			dict["rechargeTime"] = line 
-	
-		if line.startswith("Flexible", 0, 8):
+			#add requires momentum to rechargetime, in rules momentum is in its' own line but makes sense to have it here
+			#if "rechargeTime" in powerDictionary and "momentum" in powerDictionary["rechargeTime"]:
+			#	powerDictionary["rechargeTime"] = line + " (requires momentum)"
+			#else:
+			powerDictionary["rechargeTime"] = line
+
+		#flexible (fighter, bard), ranged or melee attack powers (rogue)
+		if line.startswith("Flexible", 0, 8) or line.startswith("Melee attack", 0, 12) or line.startswith("Ranged attack", 0, 13):
 			line = cleanUnicodeFromString(line)
-			dict["rechargeTime"] = line
+			powerDictionary["attackType"] = line
 			
 		#targets, such as Target: One nearby enemy
 		if line.startswith("Target", 0, 6):
 			line = cleanUnicodeFromString(line)
-			dict["target"] = removeMetaText(line)
+			powerDictionary["target"] = removeMetaText(line)
+
+		#handle Slick Feint from rogue, since it has to be special. Of course.
+		if line.startswith("First Target", 0, 12) or line.startswith("Second Target", 0, 13):
+			if "target" not in powerDictionary:
+				powerDictionary["target"] = line
+			else:
+				powerDictionary["target"] = powerDictionary["target"] + "\n" + line
 		
 		#attack roll, such as Attack: Wisdom + Level vs. PD
 		if line.startswith("Attack", 0, 6):
 			line = cleanUnicodeFromString(line)
-			dict["attackRoll"] = removeMetaText(line)
+			if "attackRoll" not in powerDictionary:
+				powerDictionary["attackRoll"] = removeMetaText(line)
+			else:
+				powerDictionary["attackRoll"] = powerDictionary["attackRoll"] + "\n" + line
 		
 		#handle hit or effect portion, they go to same field. Clerics can have Cast for Power or Cast for Broad Effect, those in here too.
 		#Bards have Opening & Sustained effect and Final Verse, add them as well
 		if line.startswith("Hit", 0, 3) or line.startswith("Effect", 0, 6) or line.startswith("Cast for", 0, 8) or line.startswith("Opening & Sustained", 0, 19) or line.startswith("Final Verse", 0, 11):
 			#print(line)
 			line = cleanUnicodeFromString(line)
-			if "hitDamageOrEffect" in dict:
-				dict["hitDamageOrEffect"] = dict["hitDamageOrEffect"] + "\n" + line
+			if "hitDamageOrEffect" in powerDictionary:
+				powerDictionary["hitDamageOrEffect"] = powerDictionary["hitDamageOrEffect"] + "\n" + line
 			else:
-				dict["hitDamageOrEffect"] = removeMetaText(line)
+				powerDictionary["hitDamageOrEffect"] = removeMetaText(line)
 				
 		#add higher level effects to the effect field as well
 		if line.startswith("3rd", 0, 3) or line.startswith("5th", 0, 3) or line.startswith("7th", 0, 3) or line.startswith("9th", 0, 3):
 			line = cleanUnicodeFromString(line)
 			print("\n" + line)
-			dict["hitDamageOrEffect"] = dict["hitDamageOrEffect"] + "\n" + line
-		
+			powerDictionary["hitDamageOrEffect"] = powerDictionary["hitDamageOrEffect"] + "\n" + line
+
+
 		if line.startswith("Miss", 0, 4):
 			line = cleanUnicodeFromString(line)
-			uprint("line starts with Miss: " + line)
-			dict["missDamage"] = removeMetaText(line)
-		
-		#lines that are only in few places, such as with chain spells, wizard's teleport shield or resurrect
-		if line.startswith("Chain", 0, 5) or line.startswith("Limited", 0, 7) or line.startswith("Always", 0, 6): 
-			line = cleanUnicodeFromString(line)
-			if "notes" in dict:
-				dict["notes"] = dict["notes"] + "\n" + line
+			#uprint("line starts with Miss: " + line)
+			# Slick feint from rogues can have multiple miss effects. Since it's special.
+			if "missDamage" not in powerDictionary:
+				line = removeMetaText(line)
+				powerDictionary["missDamage"] = line
 			else:
-				dict["notes"] = line
+				powerDictionary["missDamage"] = powerDictionary["missDamage"] + "\n" + line
+		
+		#lines that are only in few places, such as with chain spells, teleport shield, Tumbling strike or resurrect
+		if line.startswith("Chain", 0, 5) or line.startswith("Limited", 0, 7) or line.startswith("Always", 0, 6) or line.startswith("Note", 0, 4):
+			line = cleanUnicodeFromString(line)
+			if "notes" in powerDictionary:
+				powerDictionary["notes"] = powerDictionary["notes"] + "\n" + line
+			else:
+				powerDictionary["notes"] = line
 		
 		#many spells and big portion of fighter maneuvers have Special field
 		if line.startswith("Special", 0, 7):
 			line = cleanUnicodeFromString(line)
-			dict["special"] = removeMetaText(line)
+			if "special" not in powerDictionary:
+				powerDictionary["special"] = removeMetaText(line)
+			else:
+				powerDictionary["special"] = powerDictionary["special"] + "\n" + removeMetaText(line)
 		
 		#handle feats
 		if line.startswith("Adventurer", 0, 10):
 			line = cleanUnicodeFromString(line)
-			#remove the text "Adventurer Feat" from the string
+			#remove the text "Adventurer Feat " from the string
 			line = re.sub(r'.*Adventurer Feat ', '', line)
-			#don't add the first symbol, which will be whitespace since above regex only removes the text
-			dict["adventurerFeat"] = line
+			powerDictionary["adventurerFeat"] = line
 			
 		if line.startswith("Champion", 0, 8):
 			line = cleanUnicodeFromString(line)
 			line = re.sub(r'.*Champion Feat ', '', line)
-			dict["championFeat"] = line
+			powerDictionary["championFeat"] = line
 			
 		if line.startswith("Epic", 0, 5):
 			line = cleanUnicodeFromString(line)
 			line = re.sub(r'.*Epic Feat ', '', line)
-			dict["epicFeat"] = line
+			powerDictionary["epicFeat"] = line
 		
 		#for example bard spells have standard action written as casting time, line also tells sustain roll needed
 		if line.startswith("Standard action", 0, 15):
 			line = cleanUnicodeFromString(line)
-			dict["castingTime"] = line
+			powerDictionary["castingTime"] = line
 		
 		if line.startswith("Quick action", 0, 12):
 			line = cleanUnicodeFromString(line)
-			dict["castingTime"] = line
+			powerDictionary["castingTime"] = line
 			
 		if line.startswith("Move action", 0, 11):
 			line = cleanUnicodeFromString(line)
-			dict["castingTime"] = line
+			powerDictionary["castingTime"] = line
 			
 		if line.startswith("Free action", 0, 11):
 			line = cleanUnicodeFromString(line)
-			dict["castingTime"] = line	
-				
+			powerDictionary["castingTime"] = line
+
+		if line.startswith("Interrupt", 0, 9):
+			line = cleanUnicodeFromString(line)
+			powerDictionary["castingTime"] = line
+
 		#add spell group (level) that should be added at convert_spells_to_json
 		if line.startswith("spellLevel", 0, 10):
-			dict["groupName"] = removeMetaText(line)
+			powerDictionary["groupName"] = removeMetaText(line)
 		i += 1
-		
-		if line.startswith("Triggering", 0, 10):
+
+		#rogues have trigger, flexible attacks have triggering in the line
+		if line.startswith("Triggering", 0, 10) or line.startswith("Trigger", 0, 7):
 			line = cleanUnicodeFromString(line)
-			dict["trigger"] = removeMetaText(line)
+			powerDictionary["trigger"] = removeMetaText(line)
 	
 	#most spells have no casting time written, at that case it's standard action.
 	#Flexible attacks are not included, naturally.
-	if "castingTime" not in dict and "Flexible" not in dict["rechargeTime"]:
-		dict["castingTime"] = "Standard action to cast"
+	if "castingTime" not in powerDictionary:
+		#print(powerDictionary["attackType"])
+		if "attackType" in powerDictionary and ("Flexible" not in powerDictionary["attackType"] and "Melee attack" not in powerDictionary["attackType"] and "Ranged attack" not in powerDictionary["attackType"]):
+			powerDictionary["castingTime"] = "Standard action to cast"
 		
-	return dict
+	return powerDictionary
 	
 def removeMetaText(text):
 	lineText = re.sub(r'.*:', '', text)
@@ -218,9 +248,9 @@ def cleanUnicodeFromString(text):
 	return text
 
 def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
-    enc = file.encoding
-    if enc == 'UTF-8':
-        print(*objects, sep=sep, end=end, file=file)
-    else:
-        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
-        print(*map(f, objects), sep=sep, end=end, file=file)
+	enc = file.encoding
+	if enc == 'UTF-8':
+		print(*objects, sep=sep, end=end, file=file)
+	else:
+		f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+		print(*map(f, objects), sep=sep, end=end, file=file)
